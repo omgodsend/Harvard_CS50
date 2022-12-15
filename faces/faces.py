0919909d7f5526f53a3c489d7ecc88b88d62e5ae
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-15 01:46:07 +0000

    Thu, Dec 15, 2022, 1:46 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,14 +2,12 @@ def main():
     text = input("enter phrase: ")
     result = convert(text)
     print(result)
-
+main()
 
 def convert(text):
-    convert(happy)
-    print()
-
-    smile = print("\U0001F642")
-    sad = print("\U0001F641")
+    if text == ":)":
+        smile = print("\U0001F642")
+        sad = print("\U0001F641")
 
 
 print("\U0001F642")
