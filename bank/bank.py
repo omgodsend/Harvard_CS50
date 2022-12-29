8785f101de12d2480e71e00761fcef9a8433266e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-29 01:52:12 +0000

    Thu, Dec 29, 2022, 1:52 AM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,7 +2,7 @@ phrase = input("Greeting: ").lower().strip()
 
 if phrase.startswith("h") and phrase != "hello":
     print ("$20")
-elif phrase == "hello":
+elif phrase.startswith("hello"):
     print("$0")
 else:
     print("$100")
