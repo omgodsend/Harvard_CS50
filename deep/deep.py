a0947bd2263c2a761cfd66af54da211d88113297
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-29 01:08:09 +0000

    Thu, Dec 29, 2022, 1:08 AM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,6 @@
+answer = input("What is the Great answer to the meaning of life, the universe, and everything? ").lower().strip()
+
+if answer == "42" or answer == "fourty-two" or answer == "fourty two":
+    print("Yes")
+else:
+    print ("No")
