cd0e30355c15ce58a0e5e6724f022f8bbf3868c2
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-30 01:23:57 +0000

    Fri, Dec 30, 2022, 1:23 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,12 +2,18 @@ file = input("File name: ").lower().strip()
 
 if file.endswith("gif"):
     print("image/gif")
+elif file.endswith("jpg"):
+    print("image/jpg")
+elif file.endswith("jpeg"):
+    print("image/jpeg")
+elif file.endswith("png"):
+    print("image/png")
+elif file.endswith("pdf"):
+    print("image/pdf")
+elif file.endswith("txt"):
+    print("image/txt")
+elif file.endswith("zip"):
+    print("image/zip")
+else:
+    print("application/octet-stream")
 
-
-##gif
-#.jpg
-# .jpeg
-#.png
-#.pdf
-#.txt
-#.zip
