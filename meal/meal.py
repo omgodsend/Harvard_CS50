1022ae3177869affccc0897b0479383458512258
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-30 22:46:34 +0000

    Fri, Dec 30, 2022, 10:46 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,16 +5,17 @@ def main():
     convert()
 
 
-    nhours = int(hours)
-    nminutes = int(minutes)
-
     if nhours >= 7 and nhours <= 8:
         print("Breakfast time")
     elif
 
 
 def convert(time):
+    hours, minutes = time.split(":")
 
+    nhours = int(hours)
+    nminutes = int(minutes)
+    return n
 
 
 if __name__ == "__main__":
