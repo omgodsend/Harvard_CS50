1768d06e29b311cd54102525081fe24f3d6c8897
omgodsend <119549138+omgodsend@users.noreply.github.com>
2022-12-31 00:24:41 +0000

    Sat, Dec 31, 2022, 12:24 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,10 +5,10 @@ def main():
 
     if nhours >= 7 and nhours <= 8:
         print("Breakfast time")
-    elif nhours >= 12 and nhours <= 13:
+    if nhours >= 12 and nhours <= 13:
         print("Lunch time")
-    elif nhours >= 1 and nhours <= 13:
-        print("Lunch time")
+    if nhours >= 18 and nhours <= 19:
+        print("Dinner time")
 
 def convert(time):
     hours, minutes = time.split(":")
