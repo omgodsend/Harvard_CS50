e91592272313a84c522607f9407bf8768108ef79
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-01 23:54:16 +0000

    Sun, Jan 1, 2022, 11:54 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,9 +1,11 @@
 phrase = input("camelCase: ")
 
-for str().upper() in phrase:
-    print(phrase, sep="_", end="")
+for word in phrase.upper():
+    print(word, sep="_".upper())
 
-
+def word(upper):
+    if phrase == phrase.upper():
+        print(upper)
 
 
 
