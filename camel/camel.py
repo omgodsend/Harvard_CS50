cb0853556f9166bd5c982049d401a9a774f0f229
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-02 00:15:35 +0000

    Mon, Jan 2, 2023, 12:15 AM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,9 +3,7 @@ phrase = input("camelCase: ")
 print("snake_case: ")
 
 for letter in phrase:
-    print(letter, sep="_", end="")
-
-for letter in phrase
+    if letter.isupper()):
 
 while letter in phrase.isupper():
     print(letter, sep="_")
