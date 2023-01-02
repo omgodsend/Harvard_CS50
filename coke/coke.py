ae0a743594a9a2ee9920df83bf80a1ae279cbd59
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-02 16:03:05 +0000

    Mon, Jan 2, 2023, 4:03 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,7 @@
 amount_due = 50
-print("Amount Due: " + amount_due)
+print("Amount Due: " + str(amount_due))
 
 cost = int(input("Insert Coin : "))
 
-while amount_due < 50:
-    newcost -= cost
+while cost < 50:
+    amount_due -= cost
