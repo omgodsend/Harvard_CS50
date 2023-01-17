ed6fcb83d1e1bfd57e56da4c12988b797561ad25
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-17 18:45:05 +0000

    Tue, Jan 17, 2023, 6:45 PM +00:00

--- cs50/mario-more/mario.c
+++ cs50/mario-more/mario.c
@@ -1,17 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void);
+int main(void)
 
 {
-
-int n;
 
 do
 {
     int n = get_int("size: ");
 }
-while (n > 0  && n < 9)
+while (int n > 0  && int n < 9)
 
 n--;
     {
