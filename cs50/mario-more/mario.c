2f4447a18bbdce0416f152e5cf5412459ba49b1e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-17 18:42:32 +0000

    Tue, Jan 17, 2023, 6:42 PM +00:00

---
+++ cs50/mario-more/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void);
+
+{
+
+int n;
+
+do
+{
+    int n = get_int("size: ");
+}
+while (n > 0  && n < 9)
+
+n--;
+    {
+        printf("#");
+    }
+
+printf("\n");
+
+}
+
+
