44f507e27c04e0ca1402a66c6013d9ca053931ac
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-01-19 01:00:34 +0000

    Thu, Jan 19, 2023, 1:00 AM +00:00

--- cs50/mario-more/mario.c
+++ cs50/mario-more/mario.c
@@ -15,7 +15,16 @@ int main(void)
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 1; j <= (n - i); j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int k = 1; k <= i; k++)
         {
             printf("#");
         }
