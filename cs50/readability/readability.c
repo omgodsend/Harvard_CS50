3a0338ba2ae3d5941040a1b92722efc4fdd7253f
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-02-08 20:37:40 +0000

    Wed, Feb 8, 2023, 8:37 PM +00:00

---
+++ cs50/readability/readability.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+
+int main(void)
+{
+
+    string all = get_string("Text: ");
+
+    int letters = count_letters(all);
+
+    printf("%i letters\n", letters);
+
+
+}
+
+
+int count_letters(string text)
+{
+    int num = strlen(text);
+    int len = 0;
+
+    for (int i = 0; i < num; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            len ++;
+        }
+    }
+
+return len;
+}
