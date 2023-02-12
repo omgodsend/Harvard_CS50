bbeca0773c364818d553b19c89489d115f16a6d1
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-02-12 22:07:48 +0000

    Sun, Feb 12, 2023, 10:07 PM +00:00

--- cs50/caesar/caesar.c
+++ cs50/caesar/caesar.c
@@ -1,7 +1,23 @@
+#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+
+// Make sure program was run with just one command-line argument
+
+    // Make sure every character in argv[1] is a digit
+
+    // Convert argv[1] from a `string` to an `int`
+
+    // Prompt user for plaintext
+
+    // For each character in the plaintext:
+
+        // Rotate the character if it's a letter
+
 
 }
