262ca6e34965f2793402112c95ce8b664976646e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-02-25 23:22:08 +0000

    Sat, Feb 25, 2023, 6:22 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
         {
             if (strcmp(candidates[i].name, name) == 0)
             {
-                preferences[i][j] = i;
+                preferences[voter][rank] = i;
                 return true;
             }
         }
