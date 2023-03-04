f159f67467cdec2a7029440c72e4e26e03cca657
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-04 23:18:29 +0000

    Sat, Mar 4, 2023, 6:18 PM -05:00

--- cs50/plurality/plurality.c
+++ cs50/plurality/plurality.c
@@ -82,6 +82,7 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
+    int max_votes = 0
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidate[i].votes > max_votes)
