d94ad6aafb2a3ed7bcc8f3cd7d9a2bd2749a4712
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-04 23:04:51 +0000

    Sat, Mar 4, 2023, 6:04 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -146,7 +146,17 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-    
+    int ballot = voter_count;
+
+    for (int i = 0; i < candidate_count; i++)
+        {
+            
+
+
+        }
+
+
+
     return;
 }
 
