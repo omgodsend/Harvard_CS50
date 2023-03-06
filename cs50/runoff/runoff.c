1fb18c3cd908914ea18b7feb8a672c56ab560477
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-06 17:50:13 +0000

    Mon, Mar 6, 2023, 12:50 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -194,8 +194,10 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false && candidates[i].votes
-
+        if (candidates[i].eliminated == false && candidates[i].votes == min)
+        {
+        return true;
+        }
     }
     return false;
 }
@@ -203,6 +205,6 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    
     return;
 }
