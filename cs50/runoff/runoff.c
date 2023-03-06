65447b83d3dd027d375dee183fcee99aaebbbc55
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-06 17:43:43 +0000

    Mon, Mar 6, 2023, 12:43 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -181,7 +181,7 @@ int find_min(void)
     int min = voter_count;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes < min)
+        if (candidates[i].eliminated == false && candidates[i].votes < min)
         {
             min = candidates[i].votes;
         }
