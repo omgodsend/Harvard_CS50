410b3973b0c88b65620381c9bcd1524731d6a83a
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-06 16:57:21 +0000

    Mon, Mar 6, 2023, 11:57 AM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -147,9 +147,16 @@ void tabulate(void)
 {
     // TODO
     for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
         {
-            for (int j = 0; j < candidate_count; j++)
+            int candidate_index = preferences[i][j]
         }
+
+
+
+         }
+
 
     return;
 }
