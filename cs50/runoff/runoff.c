82d1316dd78edcb488b08d5f6e74e7e1e739bf0b
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-06 17:46:17 +0000

    Mon, Mar 6, 2023, 12:46 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -192,7 +192,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes
+
+    }
     return false;
 }
 
