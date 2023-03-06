6f2e99910e260e10d2033c6ad560301fba8ac203
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-06 17:11:23 +0000

    Mon, Mar 6, 2023, 12:11 PM -05:00

--- cs50/runoff/runoff.c
+++ cs50/runoff/runoff.c
@@ -164,7 +164,11 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    if candidates[i].votes > (voter_count / 2)
+    {
+        return true
+    }
     return false;
 }
 
