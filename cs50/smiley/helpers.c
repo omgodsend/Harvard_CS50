7a3299a3ef9a812be522407929cd3cde85b02900
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-10 23:15:06 +0000

    Fri, Mar 10, 2023, 6:15 PM -05:00

--- cs50/smiley/helpers.c
+++ cs50/smiley/helpers.c
@@ -3,21 +3,20 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    RGBTRIPLE pixel = image[0][0]
+    RGBTRIPLE pixel = image[0][0];
+
+    RGBTRIPLE color;
+    color.rgbtBlue = 0xFF;
+    color.rgbtGreen = 0x00;
+    color.rgbtRed = 0x80;
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE color;
-            color.rgbtBlue = 0xFF;
-            color.rgbtGreen = 0x00;
-            color.rgbtRed = 0x80;
             if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0)
             {
-                pixel.rgbtBlue[10];
-                pixel.rgbtGreen[10];
-                pixel.rgbtRed[10];
+               image[i][j] = color;
             }
         }
     }
