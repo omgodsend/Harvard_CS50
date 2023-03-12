7957c2e985a9c53e6bf1e51888d954da0fbdea7d
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-12 03:15:09 +0000

    Sat, Mar 11, 2023, 10:15 PM -05:00

--- cs50/filter-less/helpers.c
+++ cs50/filter-less/helpers.c
@@ -30,9 +30,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+            int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
             image[i][j].rgbtBlue = sepuaBlue;
-           image[i][j].rgbtGreen = sepiaGreen;
-           image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
         }
     }
     return;
