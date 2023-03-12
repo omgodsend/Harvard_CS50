debd5dad803882f2d7c90d7cba94610aa48d6c68
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-12 03:27:00 +0000

    Sat, Mar 11, 2023, 10:27 PM -05:00

--- cs50/filter-less/helpers.c
+++ cs50/filter-less/helpers.c
@@ -45,7 +45,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            image[i][j] = image[i][j]
         }
     }
     return;
