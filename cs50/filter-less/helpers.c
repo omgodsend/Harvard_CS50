44f8df93bbfbf6d12218ad29dc72159dcf441fc2
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-12 02:48:09 +0000

    Sat, Mar 11, 2023, 9:48 PM -05:00

--- cs50/filter-less/helpers.c
+++ cs50/filter-less/helpers.c
@@ -9,7 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
            int avg = ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3)
-           image[i][j]
+
+           image[i][j] 
         }
 
     return;
