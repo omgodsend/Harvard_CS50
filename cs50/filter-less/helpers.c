e398f8466c41873e05a5846f207fbda0421f075f
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-12 03:13:36 +0000

    Sat, Mar 11, 2023, 10:13 PM -05:00

--- cs50/filter-less/helpers.c
+++ cs50/filter-less/helpers.c
@@ -22,11 +22,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+    int sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+    int sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            
+            image[i][j].rgbtBlue = sepuaBlue;
+           image[i][j].rgbtGreen = sepiaGreen;
+           image[i][j].rgbtRed = sepiaRed;
         }
     }
     return;
