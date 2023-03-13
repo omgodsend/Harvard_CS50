a8a5c7fe2504e867555ac93c94bde7e4b16c902e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-13 23:34:15 +0000

    Mon, Mar 13, 2023, 7:34 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         return 0;
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
 
     }
 
