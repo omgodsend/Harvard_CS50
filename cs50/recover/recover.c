caa70307011e71222839087b8141a8575fa15c1f
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-13 23:39:14 +0000

    Mon, Mar 13, 2023, 7:39 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -6,13 +6,16 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
-    typedef uint8_t BYTE;
+    typedef struct
+    {
+    uint8_t BYTE;
+    }
 
     while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         return 0;
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0)
-        
+
     }
 
         fclose(file)
