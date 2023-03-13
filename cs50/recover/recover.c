9f003ae4e1fdef5a0c253b7c455f2558389cdc60
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-13 23:58:09 +0000

    Mon, Mar 13, 2023, 7:58 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -6,19 +6,27 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
+    if (argc != 1)
+    {
+        printf("Enter only one file\n")
+        return 1
+    }
+
     typedef struct
     {
     uint8_t BYTE;
     }byte;
 
-    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
+    while (fread(buffer, 1, byte(64), file) == BLOCK_SIZE)
     {
-        return 0;
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
-        
-    }
+
+
 
         fclose(file)
+    }
+
+
 
 
 }
