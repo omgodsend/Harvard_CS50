5555c94f0c50a44a8e262850165116dc2ea9e197
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-13 23:13:41 +0000

    Mon, Mar 13, 2023, 7:13 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -6,7 +6,11 @@ int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    {
+        return 0;
 
+    }
 
 
 
