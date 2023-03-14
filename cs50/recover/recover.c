924f6513f340b0dd5ecfcff6c6018e4a5fa66623
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 00:53:12 +0000

    Mon, Mar 13, 2023, 8:53 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -27,21 +27,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *filename = malloc(sizeof(char) * 12);
-    if (filename == NULL) {
-        printf("Error: Could not allocate memory.");
-        free(buffer);
-        fclose(file);
-        return 1;
-    }
-
     while (fread(buffer, 1, sizeof(byte) * 64, file) == byte(64))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
-            sprintf(filename, "%03i.jpg", 2)
-            FILE *img = fopen(filename, "a")
-            fwrite(data, size, number, outputr)
+            sprintf(filename, "%03i.jpg", 2);
+            FILE *img = fopen(filename, "a");
+            fwrite(data, size, number, outputr);
         }
         else
         {
