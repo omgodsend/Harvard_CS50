0fb4b3030a46ba27c2c1efa89a9780250b2daf92
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 02:54:17 +0000

    Mon, Mar 13, 2023, 10:54 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -29,11 +29,11 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, sizeof(BYTE)* 512, 1, file) == 1)
     {
-        if (buffer[0].BYTE == 0xff && buffer[1].BYTE == 0xd8 && buffer[2].BYTE == 0xff && ((buffer[3].BYTE & 0xf0) == 0xe0))
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
             if (newfile != NULL)
             {
-                fclose(newfile)
+                fclose(newfile);
             }
 
             sprintf(filename, "%03i.jpg", jpg_count++);
