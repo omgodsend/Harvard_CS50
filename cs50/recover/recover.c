ae145a4ea0dfa23acebc93e7ea092d068bb79630
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 02:26:40 +0000

    Mon, Mar 13, 2023, 10:26 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -15,9 +15,13 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        printf("Error: Could not open file %s.\n", argv[1]);
-        return 1;
+        fprintf("Error: Could not open %s.\n", argv[1]);
+        return 2;
     }
+
+    FILE* outptr = NULL;
+
+    BYTE buffer[512];
 
     byte *buffer = malloc(sizeof(byte) * 64);
     if (buffer == NULL)
