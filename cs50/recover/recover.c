398af75ded03fa7c694b9c2c30c3eea24360939d
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 00:27:18 +0000

    Mon, Mar 13, 2023, 8:27 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -2,34 +2,36 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+typedef struct
+{
+    uint8_t BYTE;
+}
+byte;
+
 int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
-    if (argc != 1 || *file == NULL)
+    if (argc != 2 || *file == NULL)
     {
         printf("ERR. Enter valid file\n")
         return 1
     }
 
-    char *size = malloc(sizeof(*file) + 1);
+    char filename
+
+    char *size = malloc(sizeof(byte) * 64);
     if (size == NULL)
     {
         return 1;
     }
 
-    typedef struct
-    {
-    uint8_t BYTE;
-    }
-    byte;
-
-    while (fread(buffer, 1, byte(64), file) == byte(64))
+    while (fread(buffer, 1, sizeof(byte) * 64, file) == byte(64))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
-            sprint(filename, "%03i.jpg", 2)
-            FILE *img = fopen(filename, "w")
+            sprintf(filename, "%03i.jpg", 2)
+            FILE *img = fopen(filename, "a")
             fwrite(data, size, number, outputr)
         }
         else
