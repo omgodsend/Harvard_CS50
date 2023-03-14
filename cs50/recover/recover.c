c0750bb750eb65081af909d6ef3b546d8c5ab437
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 00:56:35 +0000

    Mon, Mar 13, 2023, 8:56 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -23,15 +23,17 @@ int main(int argc, char *argv[])
     byte *buffer = malloc(sizeof(byte) * 64);
     if (buffer == NULL)
     {
-        printf("Error: Could not allocate memory")
+        printf("Error: Could not allocate memory");
         return 1;
     }
 
-    while (fread(buffer, 1, sizeof(byte) * 64, file) == byte(64))
+    int file_count = 0;
+
+    while (fread(buffer, 1, sizeof(byte), 64, file) == 64)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
+        if (buffer[0].BYTE == 0xff && buffer[1].BYTE == 0xd8 && buffer[2].BYTE == 0xff && ((buffer[3].BYTE & 0xf0) == 0xe0))
         {
-            sprintf(filename, "%03i.jpg", 2);
+            sprintf(filename, "%03i.jpg", file_count);
             FILE *img = fopen(filename, "a");
             fwrite(data, size, number, outputr);
         }
