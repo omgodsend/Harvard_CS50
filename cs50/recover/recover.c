d4763f98d157e33982cdab935b16e14bd7a490d5
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 01:02:59 +0000

    Mon, Mar 13, 2023, 9:02 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -16,18 +16,32 @@ int main(int argc, char *argv[])
         return 1
     }
 
-    char filename
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Error: Could not open file %s.\n", argv[1]);
+        return 1;
+    }
 
     byte *buffer = malloc(sizeof(byte) * 64);
     if (buffer == NULL)
     {
         printf("Error: Could not allocate memory");
+        fclose(file);
+        return 1;
+    }
+
+    char *filename = malloc(sizeof(char) * 12);
+    if (filename == NULL) {
+        printf("Error: Could not allocate memory.\n");
+        free(buffer);
+        fclose(file);
         return 1;
     }
 
     int file_count = 0;
 
-    while (fread(buffer, 1, sizeof(byte), 64, file) == 64)
+    while (fread(buffer, sizeof(byte), 64, file) == 64)
     {
         if (buffer[0].BYTE == 0xff && buffer[1].BYTE == 0xd8 && buffer[2].BYTE == 0xff && ((buffer[3].BYTE & 0xf0) == 0xe0))
         {
