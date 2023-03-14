63a81847f6cfad54e1ce77c2304fd64d50bf3141
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-14 00:08:50 +0000

    Mon, Mar 13, 2023, 8:08 PM -04:00

--- cs50/recover/recover.c
+++ cs50/recover/recover.c
@@ -28,10 +28,10 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0) == 0xe0))
         {
             for (int i = 0; i < x; i++)
-            FILE *file = fopen([i])
+            FILE *file = fopen([i]".jpg", "a")
         }
 
-
+        
         fclose(file)
     }
 
