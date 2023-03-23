2f45c94299fc923d36bc3e3ee0f9ff3ac11b4d37
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-23 19:53:34 +0000

    Thu, Mar 23, 2023, 3:53 PM -04:00

--- cs50/world-cup/tournament.py
+++ cs50/world-cup/tournament.py
@@ -16,6 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    f = open(sys.argv[1])
+    csv.DictReader(f)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
