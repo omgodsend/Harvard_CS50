19cb923748c3f9cf0b61a152a874830ccf3f9eb5
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-24 21:29:02 +0000

    Fri, Mar 24, 2023, 5:29 PM -04:00

--- cs50/sentimental-credit/credit.py
+++ cs50/sentimental-credit/credit.py
@@ -1,24 +1,20 @@
 from cs50 import get_int
 
-#   python credit.py
+# python credit.py
 def main():
+    card = get_int("Number: ")
 
-    card_num = []
-
-    card_num = get_int("Number: ")
-
-
-    while card_num <= 0:
+    while card <= 0:
          card = get_int("Number: ")
+
+    card = str(card)
 
     if len(card) not in [13, 15, 16]:
          print("INVALID")
 
-    card_str = str(card)
-
-    if card_str[0] == "3" and (card_str[1] == "4" or card_str[1] == "7"):
+    if card[0] == "3" and (card[1] == "4" or card[1] == "7"):
          card_type = "AMEX"
-
+    else if card
 
 def algorithm(number):
      return
