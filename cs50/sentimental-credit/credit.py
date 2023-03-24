f67c3dc1324f4c38ae23eb42b2c255546e495684
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-24 21:03:02 +0000

    Fri, Mar 24, 2023, 5:03 PM -04:00

--- cs50/sentimental-credit/credit.py
+++ cs50/sentimental-credit/credit.py
@@ -3,13 +3,18 @@ from cs50 import get_int
 def main():
     try:
         card = get_int("Number: ")
-        if n > 0:
+        if card > 0:
             return card
     except ValueError:
             print("Not and integer")
 
     if len(card) != 13 or len(card) != 15 or len(card) != 16:
          print("INVALID")
+
+    card_str = str(card)
+
+    if card_str[0] == "3" and (card_str[1] == "4" or card_str[1] == "7"):
+         card_type = "AMEX"
 
 
 def algorithm(number):
