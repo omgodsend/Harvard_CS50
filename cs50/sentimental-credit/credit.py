427aa795677f0a73eebe0454ac5f3c2a65dfaf4e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-24 20:37:29 +0000

    Fri, Mar 24, 2023, 4:37 PM -04:00

---
+++ cs50/sentimental-credit/credit.py
@@ -0,0 +1,26 @@
+
+def main()
+    while true
+        try:
+            card = int(input("Number: "))
+            if n > 0:
+                return card
+        except ValueError:
+            print("Not and integer")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
