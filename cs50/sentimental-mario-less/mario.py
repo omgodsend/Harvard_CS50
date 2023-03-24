126b003d8ca1226374127e4944a08e9297ffdaab
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-24 20:10:33 +0000

    Fri, Mar 24, 2023, 4:10 PM -04:00

--- cs50/sentimental-mario-less/mario.py
+++ cs50/sentimental-mario-less/mario.py
@@ -2,6 +2,8 @@ def main():
     height = get_height()
     for i in range(height):
         print("#", end="")
+        for i in range(height):
+            print()
 
 def get_height():
     while True:
