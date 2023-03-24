cd363915ff874d89f99af95adb6dd1709ffbcfe2
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-24 20:19:29 +0000

    Fri, Mar 24, 2023, 4:19 PM -04:00

--- cs50/sentimental-mario-less/mario.py
+++ cs50/sentimental-mario-less/mario.py
@@ -1,11 +1,14 @@
 def main():
     height = get_height()
-    for i in range(height):
-        print("#", end="")
-        print("\n")
-        for i in range(height):
-            print("#", end="")
-
+    for i in range(height//2 + 1):  # Only iterate half of the height
+        # print spaces before hashes
+        print(" " * (height - i - 1), end="")
+        # print left half of hashes
+        print("#" * (i + 1), end="")
+        # print middle gap
+        print("  ", end="")
+        # print right half of hashes
+        print("#" * (i + 1))
 
 def get_height():
     while True:
