026989a73bb64e9529756f28d99e1725a7101f55
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-25 20:22:53 +0000

    Sat, Mar 25, 2023, 4:22 PM -04:00

---
+++ cs50/sentimental-credit/test.py
@@ -0,0 +1,51 @@
+def main():
+
+    card = input("Number: ")
+
+    while not card.isdigit() or int(card) <= 0:
+         card = input("Number: ")
+
+    if validate(int(card)) == "INVALID":
+          print("INVALID")
+          sys.exit(2)
+
+    card = str(card)
+
+    if len(card) not in [13, 15, 16]:
+         print("INVALID")
+         sys.exit(1)
+
+    card_type = ""
+
+    if len(card) == 15 and card[0] == "3" and card[1] in ["4", "7"]:
+         card_type = "AMEX"
+    elif len(card) == 16 and card[0] == "5" and card[1] in ["1", "2", "3", "4", "5"]:
+         card_type = "MASTERCARD"
+    elif (len(card) == 13 or len(card) == 16) and card[0] == "4":
+         card_type = "VISA"
+    else:
+        card_type = "INVALID"
+
+    print(card_type)
+
+def validate(number):
+
+     digit_odd = 0
+     luhn_sum = 0
+
+     for i in number:
+
+          if i % 2 == 0:
+               luhn_sum += i * 2
+          else:
+               digit_odd += i
+
+     final = digit_odd + luhn_sum
+
+     if final % 10 == 0:
+          return "VALID"
+     else:
+          return "INVALID"
+
+
+main()
