926a5a0d4e0ecbe0db4fb4ae02da60a39507dda1
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-03-26 21:01:29 +0000

    Sun, Mar 26, 2023, 5:01 PM -04:00

--- cs50/sentimental-readability/readability.py
+++ cs50/sentimental-readability/readability.py
@@ -1,4 +1,11 @@
 from cs50 import get_string
 
 def main():
+
     all = get_string("Text: ")
+
+    index = 0.0588 * L - 0.296 * S - 15.8
+
+    letters = count_letters(all);
+    words = count_words(all);
+    int sent = count_sentences(all);
