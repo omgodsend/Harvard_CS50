9fe574941b8803510e70c41d1f8eb55e2009ae57
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-06-08 14:58:31 +0000

    Thu, Jun 8, 2023, 10:58 AM -04:00

--- cs50/birthdays/app.py
+++ cs50/birthdays/app.py
@@ -27,16 +27,16 @@ def index():
     if request.method == "POST":
 
         name = request.form.get("name")
-        month = request.form.get("birthday")
+        month = request.form.get("month")
+        day = request.form.get("day")
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?)", name, month, day)
-        db.execute("SELECT * FROM birthdays")
         return redirect("/")
 
         # TODO: Add the user's entry into the database
 
     else:
-
-        return render_template("index.html")
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
         # TODO: Display the entries in the database on index.html
 
 
