5a7f22e85e92fb8f2dfc83b5849f16a2e4d139d2
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-06-16 13:23:47 +0000

    Fri, Jun 16, 2023, 9:23 AM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -127,7 +127,7 @@ def register():
         if len(rows) == 1:
             return apology("username already exists")
 
-        db.execute("INSERT INTO users (username) VALUES (?)", request.form.get("username"))
+        db.execute("INSERT INTO users (username, hash) VALUES (?,?)", request.form.get("username"), request.form.get("password"))
 
     else:
         return render_template("register.html")
