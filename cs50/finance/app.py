1f69713d861fe9e3c44f2b33b53a28df69bb0844
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-06-17 14:08:35 +0000

    Sat, Jun 17, 2023, 10:08 AM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -104,7 +104,10 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
