65f5a2ee4481b8d967e15f74cd2d8183db93d60b
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-07-10 00:09:27 +0000

    Sun, Jul 9, 2023, 8:09 PM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -93,9 +93,9 @@ def buy():
         db.execute("UPDATE users SET cash = ? WHERE id = ?", new_cash, user_id)
 
         db.execute(
-            "INSERT INTO purchases (user_id, symbol, price, shares, shares_sold, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
-            user_id, symbol, stock["price"], int(shares), int(shares_sold), buy_time)
-
+            "INSERT INTO purchases (user_id, symbol, price, shares, shares_sold, timestamp) VALUES (?, ?, ?, ?)",
+            user_id, symbol, stock["price"], int(shares))
+        
         flash("Bought!")
         return redirect("/")
 
