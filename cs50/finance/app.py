a830d81d5e761d057bdbbcc8b2efc2f23ac644e0
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-07-12 16:17:59 +0000

    Wed, Jul 12, 2023, 12:17 PM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -293,12 +293,11 @@ def sell():
 
         remaining_shares = shares - shares_req
 
-        if remaining_shares == 0:
-            # If no remaining shares, delete the row from the purchases table
-            db.execute("DELETE FROM purchases WHERE symbol = ? AND user_id = ?", session["user_id"], symbol)
+        if remaining_shares > 0:
+            db.execute("UPDATE purchases SET shares = ? WHERE user_id = ? AND symbol = ?", remaining_shares, session["user_id"], symbol)
         else:
             # Update the shares in the purchases table with the remaining_shares value
-            db.execute("UPDATE purchases SET shares = ? WHERE user_id = ? AND symbol = ?", remaining_shares, session["user_id"], symbol)
+            db.execute("DELETE FROM purchases WHERE symbol = ? AND user_id = ?", session["user_id"], symbol)
 
         total_cost = int(shares_req) * stock["price"]
 
