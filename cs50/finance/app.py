75ae54ac62f983a9017633c9e0dad3c154b1c590
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-07-12 14:47:40 +0000

    Wed, Jul 12, 2023, 10:47 AM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -2,7 +2,7 @@ import os
 import datetime
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, get_flashed_messages
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
