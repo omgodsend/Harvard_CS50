024f7c1cf94e192f377cf6f2f7fbadfc6e6e2395
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-07-12 23:04:31 +0000

    Wed, Jul 12, 2023, 7:04 PM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 
+import s
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, get_flashed_messages
 from flask_session import Session
