1e4e040b503a0947afc1ae597cd9ab2a7702eb2e
omgodsend <119549138+omgodsend@users.noreply.github.com>
2023-07-12 17:17:29 +0000

    Wed, Jul 12, 2023, 1:17 PM -04:00

--- cs50/finance/app.py
+++ cs50/finance/app.py
@@ -94,8 +94,8 @@ def buy():
         db.execute("UPDATE users SET cash = ? WHERE id = ?", new_cash, user_id)
 
         db.execute(
-            "UPDATE purchases SET symbol = ?, price = ?, shares = ?, WHERE user_id = ?",
-            symbol, stock["price"], int(shares), user_id)
+            "UPDATE purchases SET price = ?, shares = ? WHERE user_id = ? AND symbol = ?",
+             stock["price"], int(shares), user_id, symbol)
 
         db.execute(
             "INSERT INTO history (user_id, symbol, price, shares, shares_bought, shares_sold, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)",
